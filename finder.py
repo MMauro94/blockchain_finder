887914f8303ee299a102c66fdc0890ad6605413d
Francesco Benvenuto <francesco_benvenuto@me.com>
2019-04-11 18:44:42 +0200

    introduced mysql querying

--- finder.py
+++ finder.py
@@ -4,6 +4,9 @@ import os
 import redis
 import pickle
 
+
+import mysql.connector
+
 from collections import deque
 from flask import Flask, request, render_template, g
 from blockchain_parser.blockchain import Blockchain
@@ -13,6 +16,30 @@ CACHE_RANGE = 200
 # first transaction 4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b
 app = Flask(__name__)
 red = redis.Redis(host="localhost", port=6379, db=0)
+red.flushall()
+
+blockchain = Blockchain("datas")
+
+# force the creation of the index
+if os.path.exists("super-big-index.pickle"):
+    os.remove("super-big-index.pickle")
+
+print("Creating index")
+next(blockchain.get_ordered_blocks("datas/index", cache="super-big-index.pickle"))
+print("Index created")
+
+
+mysql_conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    database="Blockchain",
+    passwd="toor",
+    auth_plugin="mysql_native_password",
+)
+
+
+class IllegalState(Exception):
+    pass
 
 
 @app.before_request
@@ -21,44 +48,47 @@ def before_request():
     g.request_time = lambda: "%.5fs" % (time.time() - g.request_start_time)
 
 
-def all_transactions():
+def get_block_transactions(block_height):
     blockchain = Blockchain("datas")
 
     for block in blockchain.get_ordered_blocks(
-        "datas/index", end=542000, start=545289, cache="index-cache.pickle"
+        "datas/index",
+        end=block_height,
+        start=block_height + 1,
+        cache="super-big-index.pickle",
     ):
+        print(block.height)
         for tx in block.transactions:
             yield tx
 
 
 # return transaction and list of the near ones
 def load_transaction(transaction_id):
-    cached_values = deque()
-    found = None
-    iter_transaction = all_transactions()
+    cursor = mysql_conn.cursor()
+    cursor.execute("SELECT block FROM transaction WHERE id = %s", (transaction_id,))
+    block_tuple = cursor.fetchone()
 
-    for tx in iter_transaction:
-        if len(cached_values) > CACHE_RANGE // 2:
-            cached_values.popleft()
-        cached_values.append(tx)
+    if block_tuple is None:
+        return None
+    block_it = get_block_transactions(block_tuple[0])
+
+    found = None
+    for tx in block_it:
+        marshaled_tx = pickle.dumps(tx)
+        redis_set(tx.hash, marshaled_tx)
+
         if tx.hash == transaction_id:
             found = tx
-            break
-    tx_index = 0
 
     if found is None:
-        return None
-
-    while tx_index < CACHE_RANGE // 2 and iter_transaction:
-        cached_values.append(next(iter_transaction))
-        tx_index += 1
-    return found, cached_values
+        raise IllegalState
+    return found
 
 
 def redis_get(key):
     try:
         redis_value = red.get(key)
-    except redis.exceptions.ConnectionError as err:
+    except redis.exceptions.ConnectionError:
         redis_value = None
 
     return redis_value
@@ -67,7 +97,7 @@ def redis_get(key):
 def redis_set(key, value):
     try:
         red.set(key, value)
-    except redis.exceptions.ConnectionError as err:
+    except redis.exceptions.ConnectionError:
         pass
 
 
@@ -82,17 +112,7 @@ def find_transaction(transaction_id):
     elif redis_value is not None:
         return pickle.loads(redis_value)
 
-    res = load_transaction(transaction_id)
-
-    if res is None:
-        redis_set(redis_key, "")
-        return None
-    else:
-        found, to_be_cached = res
-        for tx in to_be_cached:
-            marshaled_tx = pickle.dumps(tx)
-            redis_set(tx.hash, marshaled_tx)
-        return found
+    return load_transaction(transaction_id)
 
 
 @app.route("/")
@@ -102,8 +122,8 @@ def hello():
 
 @app.route("/search", methods=["GET"])
 def print_transaction_view():
-    t = request.values.get("t", 0)
     id_tx = request.args.get("tx_id")
+    print(id_tx)
     if id_tx is None:
         return "Error"
     res = find_transaction(id_tx)
